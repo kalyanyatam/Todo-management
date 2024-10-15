42c52f22b6e470ba268da6e465bc00a8a4ff848e
kalyan yatam <kalyanyatam09@gmail.com>
2024-10-15 19:56:38 +0530

    test

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,14 +1,30 @@
 import React from "react";
 
-const TodoList = ({ todos,deleteHandler }) => {
+const TodoList = ({ todos, deleteHandler, toggleComplete, editHandler }) => {
   return (
-    <div>
+    <ul>
       {todos.map((todo, index) => (
-        <div key={index}>
-          <h5>{todo}&nbsp; <button onClick={() => deleteHandler(index)}>Delete</button></h5>
-        </div>
+        <li key={index} style={{ textDecoration: todo.isCompleted ? 'line-through' : 'none' }}>
+          {/* Display task details instead of the entire object */}
+          <div>
+            <strong>Task:</strong> {todo.task}
+          </div>
+          <div>
+            <strong>Priority:</strong> {todo.priority}
+          </div>
+          <div>
+            <strong>Due Date:</strong> {todo.dueDate}
+          </div>
+
+          {/* Buttons for task actions */}
+          <button onClick={() => toggleComplete(index)}>
+            {todo.isCompleted ? "Undo" : "Complete"}
+          </button>
+          <button onClick={() => editHandler(index)}>Edit</button>
+          <button onClick={() => deleteHandler(index)}>Delete</button>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
